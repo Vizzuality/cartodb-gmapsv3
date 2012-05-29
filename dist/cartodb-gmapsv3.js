cd30491340a3977b3d4463197c4eb02b635fbac1
Javier de la Torre <jatorre@gmail.com>
2012-05-29 14:10:27 -0400

    correct a bug when the username is not set so that the lib can be used with cloudfront

--- dist/cartodb-gmapsv3.js
+++ dist/cartodb-gmapsv3.js
@@ -580,13 +580,19 @@ if (typeof(google.maps.CartoDBLayer) === "undefined") {
      * Generate a URL about sql api or tile api
      * @params {String} Type of url
      */  
-    CartoDBLayer.prototype._generateUrl = function(type) {
-      if (type == "sql") {
-        return this.options.sql_protocol + "://" + this.options.user_name + "." + this.options.sql_domain + ((this.options.sql_port != "") ? (":" + this.options.sql_port) : "");
-      } else {
-        return this.options.tiler_protocol + "://" + this.options.user_name + "." + this.options.tiler_domain + ((this.options.tiler_port != "") ? (":" + this.options.tiler_port) : "");
-      }
-    }
+     CartoDBLayer.prototype._generateUrl = function(type) {
+       if (type == "sql") {
+         return this.options.sql_protocol + 
+             "://" + ((this.options.user_name)?this.options.user_name+".":"")  + 
+             this.options.sql_domain + 
+             ((this.options.sql_port != "") ? (":" + this.options.sql_port) : "");
+       } else {
+         return this.options.tiler_protocol + 
+             "://" + ((this.options.user_name)?this.options.user_name+".":"")  + 
+             this.options.tiler_domain + 
+             ((this.options.tiler_port != "") ? (":" + this.options.tiler_port) : "");
+       }
+     }
 
     /**
      * Parse URI
